004f725b01227553720334fc03cf5d6cb4836f29
Mihnea Jianu <mihneasebastian.jianu@gmail.com>
2026-02-19 19:54:36 +0200

    Modified the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build/
+mkdir bin
 cd build
 make
 cp -r minesweeper ../resources/sprites ../resources/settings.txt ../resources/personal_best.txt ../bin
