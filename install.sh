9d4ea81e6ba83f79b2b7ab09b017964e8433a3d8
Mihnea Jianu <mihneasebastian.jianu@gmail.com>
2026-02-19 20:39:58 +0200

    Modified install script

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+sudo apt update
+sudo apt install libwayland-dev libxkbcommon-dev xorg-dev
+
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build/
+
 mkdir bin
+
 cd build
+
 make
+
 cp -r minesweeper ../resources/sprites ../resources/settings.txt ../resources/personal_best.txt ../bin
