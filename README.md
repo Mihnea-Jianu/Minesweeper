2b527b806dae5aa19b72cf430b26bea69676fce4
Mihnea Jianu <mihneasebastian.jianu@gmail.com>
2026-02-20 02:27:46 +0200

    Minor tweaks of install script

--- README.md
+++ README.md
@@ -4,16 +4,17 @@
 
 First, make sure the following programs are installed on your computer:
 
-- git
-- make
-- cmake
-- xorg-dev
-- libwayland-dev 
-- libxkbcommon-dev 
+    - git
+    - bash
+    - make
+    - cmake
+    - xorg-dev
+    - libwayland-dev 
+    - libxkbcommon-dev 
 
 The last three are necessarry for the compilation of GLFW.
 
-Next, you need to clone this repository to your computer. Make sure to clone recursively, as the project uses GLFW as a submodule that gets pulled from https://github.com/glfw/glfw.
+Next, you need to clone this repository to your computer. Make sure to clone recursively, as the repository uses GLFW as a submodule that gets pulled from https://github.com/glfw/glfw.
 
 To do that, open a terminal inside the folder into which to clone the repository and run:
 
@@ -22,3 +23,5 @@ To do that, open a terminal inside the folder into which to clone the repository
 Then, you can simply run the install script, however I encourage you to have a look at exactly what it does:
 
     ./install.sh
+
+After you have done that, the exectuable as well as all its resources should be inside the bin folder.
--- install.sh
+++ install.sh
@@ -1,8 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+
+# CMake will generate the necessarry Makefiles or project files with which to compile the program. 
+
+# On Linux-based systems, this will create a Makefile inside the build folder.
+# On Windows systems, this will create a Visual Studio solution inside the build folder.
+
+# It is important to note that we are not only compiling the sources proprietary to our program,
+# but also the GLFW library, which is added through git as a submodule of this repository.
+
+# For this reason, it is necessarry to specify a few options for compiling GLFW:
+# GLFW_BUILD_EXAMPLES, GLFW_BUILD_TESTS and GLFW_BUILD_DOCS, all set to OFF.
+
+# This is because we are not interested in building the examples, tests or docs
+# for GLFW, we are simply interested in compiling the library binary.
 
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build/
+
+# Once the Makefiles or project files have been generated, the program can be compiled.
+# CMake can handle the compilation for us, regardless of what method is used.
+
 cmake --build build/
 
-mkdir bin/
+# After the binary of the program has been generated, we would like to have it easily
+# accessible inside the bin folder, which we create here.
 
-cp -r build/minesweeper resources/sprites/ resources/settings.txt resources/personal_best.txt bin/
+mkdir bin/ &> /dev/null
+
+# Everything in the resources folder will be required to stay alongside the executable for it to work.
+
+cp -r resources/* bin/ &> /dev/null
+
+# This is where the executable itself is copied to the bin folder.
+# Below are all the possible locations where it can be found.
+
+cp build/minesweeper bin/ &> /dev/null
+cp build/Debug/minesweeper.exe bin/ &> /dev/null
