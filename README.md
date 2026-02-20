66ac9275e5a3e3431894843fae89bac19ce44b50
Mihnea Jianu <mihneasebastian.jianu@gmail.com>
2026-02-20 15:17:06 +0200

    Added Windows installation dependencies list, restructured installation guide

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Minesweeper
 
-## Linux Installation
+## Windows Dependencies
 
-First, make sure the following programs are installed on your computer:
+Make sure the following programs are installed on your computer:
+
+    - Git for Windows (which comes with Git Bash)
+    - CMake for Windows
+    - Microsoft Visual Studio
+
+## Linux Dependencies
+
+Make sure the following programs are installed on your computer:
 
     - git
     - bash
@@ -12,9 +20,11 @@ First, make sure the following programs are installed on your computer:
     - libwayland-dev 
     - libxkbcommon-dev 
 
-The last three are necessarry for the compilation of GLFW.
+The last three are development packages necessarry for the compilation of GLFW.
 
-Next, you need to clone this repository to your computer. Make sure to clone recursively, as the repository uses GLFW as a submodule that gets pulled from https://github.com/glfw/glfw.
+## Installation
+
+First, you need to clone this repository to your computer. Make sure to clone recursively, as the repository uses GLFW as a submodule that gets pulled from https://github.com/glfw/glfw.
 
 To do that, open a terminal inside the folder into which to clone the repository and run:
 
--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# CMake will generate the necessarry Makefiles or project files with which to compile the program. 
+# CMake will generate the necessarry makefiles or project files with which to compile the program on your system. 
 
-# On Linux-based systems, this will create a Makefile inside the build folder.
+# On Linux-based systems, this will create a makefile inside the build folder.
 # On Windows systems, this will create a Visual Studio solution inside the build folder.
 
 # It is important to note that we are not only compiling the sources proprietary to our program,
 # but also the GLFW library, which is added through git as a submodule of this repository.
 
 # For this reason, it is necessarry to specify a few options for compiling GLFW:
-# GLFW_BUILD_EXAMPLES, GLFW_BUILD_TESTS and GLFW_BUILD_DOCS, all set to OFF.
+# GLFW_BUILD_EXAMPLES, GLFW_BUILD_TESTS and GLFW_BUILD_DOCS are all set to OFF.
 
-# This is because we are not interested in building the examples, tests or docs
-# for GLFW, we are simply interested in compiling the library binary.
+# This is because we are not interested in building the examples, tests or
+# docs for GLFW, we are simply interested in compiling the library binary.
 
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build/
 
-# Once the Makefiles or project files have been generated, the program can be compiled.
-# CMake can handle the compilation for us, regardless of what method is used.
+# Once the makefiles or project files have been generated, the program can be compiled.
+# CMake can handle the compilation for us, regardless of what method and system is used.
 
 cmake --build build/
 
